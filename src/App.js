7049dc6136a6f97149b807f19801b8e05e772df0
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-10-01 22:54:35 -0400

    feat: proj init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Stars from "./components/Stars";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Star Rating Component</h1>
+      </header>
+      <Stars />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Stars.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Stars.module.css";
+
+function Stars() {
+  return <div>Stars</div>;
+}
+
+export default Stars;
